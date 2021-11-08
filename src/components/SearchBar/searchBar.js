52f38c4d512c20dfb37af0e650c267bc1fb669f4
vagrant <markscarna@gmail.com>
2021-11-08 13:20:42 -0600

    added clear btn

--- src/components/SearchBar/searchBar.js
+++ src/components/SearchBar/searchBar.js
@@ -2,15 +2,31 @@ import React, { useState } from 'react'
 import './searchBar.css'
 
 const SearchBar = () => {
-  const [searchValue, setSearchValue] = useState('the search value')
-  const handleInputChange = (event) => {
+  const [searchValue, setSearchValue] = useState('')
+  const handleInputChange = event => {
     setSearchValue(event.target.value)
   }
+  const handleClearClick = () => {
+    setSearchValue('')
+  }
+
+  const shouldDisplayButton = searchValue.length > 0
+//   console.log(shouldDisplayButton)
+
+//   let showButton = () => {
+//     if (shouldDisplayButton === true) {
+//       return <button onClick={handleClearClick}>Clear</button>
+//     }
+//   }
 
   return (
     <div>
-      <input type='text' value={searchValue} onChange={handleInputChange}
+      <input
+        type='text'
+        value={searchValue}
+        onChange={handleInputChange}
       ></input>
+      {shouldDisplayButton && <button onClick={handleClearClick}>Clear</button>}
     </div>
   )
 }
